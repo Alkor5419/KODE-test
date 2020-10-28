a3f4c254601cd5f9801e0ed37ce404cd99653cd9
Telazhevich Artur <artur_z62@mail.ru>
2020-10-28 13:32:09 +0200

    Updates

---
+++ precache-manifest.c39e668a83b3600b093d279eff564559.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef65d2c1e180cb907ab20514bbc65d99",
+    "url": "/KODE-test/index.html"
+  },
+  {
+    "revision": "aa04ee26bd47110bd457",
+    "url": "/KODE-test/static/css/main.5c6a677d.chunk.css"
+  },
+  {
+    "revision": "0787e8bf2d81c891d754",
+    "url": "/KODE-test/static/js/2.3eebfdbe.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/KODE-test/static/js/2.3eebfdbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa04ee26bd47110bd457",
+    "url": "/KODE-test/static/js/main.a172209b.chunk.js"
+  },
+  {
+    "revision": "b4d8d51e4375bcdb0175",
+    "url": "/KODE-test/static/js/runtime-main.b8fef612.js"
+  }
+]);
